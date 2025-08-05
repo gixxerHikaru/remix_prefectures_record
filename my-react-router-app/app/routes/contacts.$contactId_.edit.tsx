c8fa31d5fd5e0563ed25c2b76497422632fe409b
gixxerHikaru <yuki.sakurai0617@gmail.com>
2025-08-05 21:55:41 +0900

    編集画面の編集

--- my-react-router-app/app/routes/contacts.$contactId_.edit.tsx
+++ my-react-router-app/app/routes/contacts.$contactId_.edit.tsx
@@ -33,40 +33,8 @@ export default function EditContact() {
     <Form key={contact.id} id="contact-form" method="post">
       <p>
         <span>Name</span>
-        <input
-          aria-label="First name"
-          defaultValue={contact.first}
-          name="first"
-          placeholder="First"
-          type="text"
-        />
-        <input
-          aria-label="Last name"
-          defaultValue={contact.last}
-          name="last"
-          placeholder="Last"
-          type="text"
-        />
+        <span>{contact.first}{contact.last}</span>
       </p>
-      <label>
-        <span>Twitter</span>
-        <input
-          defaultValue={contact.twitter}
-          name="twitter"
-          placeholder="@jack"
-          type="text"
-        />
-      </label>
-      <label>
-        <span>Avatar URL</span>
-        <input
-          aria-label="Avatar URL"
-          defaultValue={contact.avatar}
-          name="avatar"
-          placeholder="https://example.com/avatar.jpg"
-          type="text"
-        />
-      </label>
       <label>
         <span>Notes</span>
         <textarea
